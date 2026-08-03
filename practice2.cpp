37cdfbab3f25fb01d4d4e57bb2a066bc7a49a6a0
Ayush14122005 <ayushsharma141205@gmail.com>
2026-08-03 11:50:08 +0530

    point

--- practice2.cpp
+++ practice2.cpp
@@ -6,27 +6,46 @@ class point{
     private:
     int x,y;
     public:
-    point(int a,int b){
-       x=a;
-       y=b;
+    point() : x{0} , y{0} {
+    cout<<"Default constructor"<<endl;}
+    point(int p, int q) : x{p},y{q}{
+   cout<<"Parametrized constructor"<<endl;
+    }
+    // point(int a,int b){
+    //    x=a;
+    //    y=b;
+    // }
+
+    // point(){
+    //     x=10;
+    //     y=20;
+    // } 
+    point add(point q){
+        point r;
+        r.x=x+q.x;
+        r.y=y+q.y;
+        return r;
     }
 
-    point(){
-        x=10;
-        y=20;
-    } 
-
     void show(){
-        cout<<x<<endl;
-        cout<<y<<endl;
+        cout<<x<<","<<y<<endl;
+        
     }
 };
 int main(){
-    int a,b;
-    cin>>a>>b;
-    point c(a,b);
-    c.show();
-    point d;
-    d.show();
+    // int a,b;
+    // cin>>a>>b;
+    // point p(a,b);
+    // p.show();
+    // point q;
+    // q.show();
+    // point r= p.add(q);
+    // r.show();
+     point p(5,10),q(20,50);
+     p.show();
+     q.show();
+     point r =p.add(q);
+     r.show();
+     return 0;
 
 }
