14a355c40819c140ee39f4b0c5c81e6becc5963f
Ayush14122005 <ayushsharma141205@gmail.com>
2026-08-03 12:01:12 +0530

    nameless object creation

--- practice2.cpp
+++ practice2.cpp
@@ -1,51 +1,44 @@
-//  define a class point  with two private paramter show them by help of constructor
-#include <bits/stdc++.h>
+/*Create a point class with two private parameters and show them with constructor*/
+#include<bits/stdc++.h>
 using namespace std;
 
-class point{
+class Point{
     private:
-    int x,y;
+    int x;
+    int y;
     public:
-    point() : x{0} , y{0} {
-    cout<<"Default constructor"<<endl;}
-    point(int p, int q) : x{p},y{q}{
-   cout<<"Parametrized constructor"<<endl;
-    }
-    // point(int a,int b){
-    //    x=a;
-    //    y=b;
-    // }
 
-    // point(){
-    //     x=10;
-    //     y=20;
-    // } 
-    point add(point q){
-        point r;
-        r.x=x+q.x;
-        r.y=y+q.y;
-        return r;
+    Point() : x{0},y{0}{
+        cout<<"Default constructer "<<endl;
+    }  //list initialization
+    Point(int p,int q): x{p},y{q}{
+        cout<<"Parameterized constructor "<<endl;
     }
+
 
     void show(){
-        cout<<x<<","<<y<<endl;
+        cout<<x<<" , "<<y<<endl;
         
     }
-};
-int main(){
-    // int a,b;
-    // cin>>a>>b;
-    // point p(a,b);
-    // p.show();
-    // point q;
-    // q.show();
-    // point r= p.add(q);
-    // r.show();
-     point p(5,10),q(20,50);
-     p.show();
-     q.show();
-     point r =p.add(q);
-     r.show();
-     return 0;
 
+    // Point add (Point q){
+    //     Point r;
+    //     r.x=x+q.x;
+    //     r.y=y+q.y;
+    //     return r;
+    // }
+    Point add (Point q){                                  
+        return Point(x + q.x , y + q.y);                  //nameless object creation
+    }
+
+
+};
+
+int main(){
+    Point p(10,20),q(20,50);
+    p.show();
+    q.show();
+    Point r = p.add(q);
+    r.show();
+    return 0;
 }
