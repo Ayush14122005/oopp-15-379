e0b7d273e76fb0ca576872f2fa22754316c39cad
Ayush14122005 <ayushsharma141205@gmail.com>
2026-08-16 23:10:16 +0530

    prg 4

---
+++ prg4.cpp
@@ -0,0 +1,23 @@
+//04. Write a program using modern C++ features such as auto and range-based for loop to traverse and 
+ 
+ //display elements of a collection. 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> numbers = {10, 20, 30, 40, 50};
+
+    cout << "Elements of Vector:" << endl;
+
+    // Range-Based For Loop with auto
+    for (auto num : numbers)
+    {
+        cout << num << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
