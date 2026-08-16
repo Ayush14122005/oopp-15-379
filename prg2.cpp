d5fd66c51ee86c915fed27426e190af5fd73f872
Ayush14122005 <ayushsharma141205@gmail.com>
2026-08-16 23:08:08 +0530

    prg 2

--- prg2.cpp
+++ prg2.cpp
@@ -1,52 +1,45 @@
+// 02. Develop a program to demonstrate parameter passing techniques (call by value, reference, and 
+//address) and analyze their effects on variable modification.
+
+
 #include <iostream>
 using namespace std;
-void show(int x, int y)
-{
-    cout << "x = " << x << ", y = " << y << endl;
-}
-void swapByValue(int x, int y)
-{
-    int temp= x;
-    x = y;
-    y =temp;
 
-    cout << "After swapping Value ";
-    show(x, y);
-}
-void swapByReference(int &x, int &y)
+// Call by Value
+void value(int x)
 {
-    int temp = x;
-    x = y;
-    y =temp;
+    x = x + 10;
+    cout << "Inside Call by Value: " << x << endl;
 }
-void swapByAddress(int *x, int *y)
+
+// Call by Reference
+void reference(int &x)
 {
-    int temp = *x;
-    *x = *y;
-    *y =temp;
+    x = x + 10;
+    cout << "Inside Call by Reference: " << x << endl;
 }
+
+// Call by Address
+void address(int *x)
+{
+    *x = *x + 10;
+    cout << "Inside Call by Address: " << *x << endl;
+}
+
 int main()
 {
-    int x, y;
-cout << "Enter two numbers: ";
-    cin >> x >> y;
-    cout <<"Call by Value"<< endl;
-    cout <<"Before swap:";
-    show(x, y);
-swapByValue(x, y);
-cout <<"After swap:";
-    show(x, y);
-    cout <<"Call by Reference"<< endl;
-    cout <<"Before swap:";
-    show(x, y);
-swapByReference(x, y);
-cout <<"After swap:";
-    show(x, y);
-    cout <<"Call by Address"<< endl;
-    cout <<"Before swap:";
-    show(x, y);
-swapByAddress(&x, &y);
-cout <<"After swap:";
-    show(x, y);
-return 0;
+    int num = 20;
+
+    cout << "Original Value = " << num << endl;
+
+    value(num);
+    cout << "After Call by Value = " << num << endl;
+
+    reference(num);
+    cout << "After Call by Reference = " << num << endl;
+
+    address(&num);
+    cout << "After Call by Address = " << num << endl;
+
+    return 0;
 }
